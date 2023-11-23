7c92f6e035155cbe79e263550009b31b1a3235fb
Carlos-Teixeira-Jr <teixeirahist1988@gmail.com>
2023-11-23 19:51:48 -0300

    feat: implementing highlight ad logic on create owner and highlight property routes

--- src/common/schemas/Owner.schema.ts
+++ src/common/schemas/Owner.schema.ts
@@ -14,6 +14,7 @@ export interface IOwner extends BaseModel, Document {
   }[]
   plan: Schema.Types.ObjectId
   userId: Schema.Types.ObjectId
+  highlightAd: number
   adCredits: number
   customerId: string
   creditCardInfo: {
@@ -40,6 +41,7 @@ export const OwnerSchema = new Schema(
     plan: { type: Schema.Types.ObjectId, ref: 'plan' },
     userId: { type: Schema.Types.ObjectId },
     adCredits: { type: Number },
+    highLightAd: { type: Number },
     customerId: { type: String },
     creditCardInfo: {
       creditCardNumber: { type: String },
