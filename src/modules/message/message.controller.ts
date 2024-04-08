2ded82e0ad317990d8acb52b6549d6ceb9291dc1
unknown <teixeirahist1988@gmail.com>
2024-04-08 08:55:04 -0300

    fix: limit of messages by property id increased and api decorators on controller

--- src/modules/message/message.controller.ts
+++ src/modules/message/message.controller.ts
@@ -1,12 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common'
 import { InjectorLoggerService } from 'modules/logger/InjectorLoggerService'
 import { LoggerService } from 'modules/logger/logger.service'
-import { IMessagesWithPagination, MessageService } from './message.service'
+import {
+  IMessagesByPropIdOut,
+  IMessagesWithPagination,
+  MessageService,
+} from './message.service'
 import { IMessageOwner } from 'common/schemas/Message_owner.schema'
 import { CreateMessageDto } from './dto/create-message.dto'
 import { FindByPropertyIdDto } from './dto/find-by-prperty-id.dto'
 import { GetAllByOwnerIdDto } from './dto/get-all-by-owner-id.dto'
+import { ApiOperation, ApiTags } from '@nestjs/swagger'
 
+@ApiTags('messages')
 @Controller('message')
 export class MessageController {
   constructor(
@@ -23,17 +29,29 @@ export class MessageController {
     return this.messageService.createOne(createMessageDto)
   }
 
+  @ApiOperation({
+    summary: 'Return all the messages related to a specific owner.',
+  })
   @Post('find-all-by-ownerId')
   async findAllByOwnerId(
     @Body() getAllByOwnerIdDto: GetAllByOwnerIdDto,
   ): Promise<IMessagesWithPagination> {
-    this.logger.log({}, 'findAll')
+    this.logger.log({}, 'find all by owner id > [controller]')
 
     return await this.messageService.findAllByOwnerId(getAllByOwnerIdDto)
   }
 
+  @ApiOperation({
+    summary: 'Return all the messages related to a specific property.',
+  })
   @Post('find-by-propertyId')
-  async findByPropertyId(@Body() findByPropertyIdDto: FindByPropertyIdDto) {
+  async findByPropertyId(
+    @Body() findByPropertyIdDto: FindByPropertyIdDto,
+  ): Promise<IMessagesByPropIdOut> {
+    this.logger.log(
+      { findByPropertyIdDto },
+      'start find messages by property id > [controller]',
+    )
     return await this.messageService.findByPropertyId(findByPropertyIdDto)
   }
 }
